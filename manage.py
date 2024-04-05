2c39575aa791ba4c466aaa5824c8a85686739196
EC2 Default User <ec2-user@ip-172-31-28-122.eu-west-1.compute.internal>
2024-04-05 18:25:36 +0000

    pylint

--- manage.py
+++ manage.py
@@ -3,8 +3,10 @@
 import os
 import sys
 
-
 def main():
+    """
+    Main function to execute Django management commands from the command line.
+    """
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hotel.settings')
     try:
         from django.core.management import execute_from_command_line
@@ -15,7 +17,6 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-
 
 if __name__ == '__main__':
     main()
